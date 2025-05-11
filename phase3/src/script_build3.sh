989191cb5e4638150c3220d051b50fca7bc91dcd
FlaviusMarian27 <marianflavius27@yahoo.com>
2025-05-11 18:37:03 +0300

    Modificare script

--- phase3/src/script_build3.sh
+++ phase3/src/script_build3.sh
@@ -8,7 +8,7 @@ gcc treasure_hub.c \
     view_treasure.c \
     ../../phase1/src/list.c \
     ../../phase1/src/view.c \
-    -o p
+    -o treasure_hub
 
 gcc monitor.c \
     ../../phase1/src/list.c \
